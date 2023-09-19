01d2e2d787c90679739014fcedfba3c68a2cbd7f
LucasGuzella <74662331+LucasGuzella@users.noreply.github.com>
2023-09-19 17:11:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Este script é usado para adicionar comportamento de rolagem suave a links âncora
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
