285e5aa05a617344442786b8105821aa211ced2e
LucasGuzella <74662331+LucasGuzella@users.noreply.github.com>
2023-09-19 17:51:49 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
     var menu = document.getElementById("menu");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "block";
-    } else {
+    if (menu.style.display === "block") {
         menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
     }
 }
